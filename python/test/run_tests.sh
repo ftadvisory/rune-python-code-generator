044975096a9b21b95a2be7156d4769493c988e10
Plamen Neykov <plamen.neykov@cloudrisk.co.uk>
2024-06-07 20:34:04 +0100

    moved the stand-alone runtime tests in their own directory

--- python/test/run_tests.sh
+++ python/test/run_tests.sh
@@ -3,3 +3,4 @@ MYPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $MYPATH
 ./rosetta_tests/run_rosetta_tests.sh
 ./cdm_tests/run_cdm_tests.sh
+./runtime_tests/run_runtime_tests.sh
