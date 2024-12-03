e2a95ac103a73a96b18841eb712b59abcefcd653
dschwartznyc <daniel.schwartz@ftadvisory.co>
2024-12-03 15:39:01 -0500

    clean up tests

--- python/test/run_tests.sh
+++ python/test/run_tests.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
+type -P python > /dev/null && PYEXE=python || PYEXE=python3
+if ! $PYEXE -c 'import sys; assert sys.version_info >= (3,10)' > /dev/null 2>&1; then
+        echo "Found $($PYEXE -V)"
+        echo "Expecting at least python 3.10 - exiting!"
+        exit 1
+fi
+
+export PYTHONDONTWRITEBYTECODE=1
+
+ACDIR=$($PYEXE -c "import sys;print('Scripts' if sys.platform.startswith('win') else 'bin')")
+$PYEXE -m venv --clear .pytest
+source .pytest/$ACDIR/activate
+
 MYPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROSETTARUNTIMEDIR="../src/main/resources/runtime"
+PYTHONCDMDIR="../target/python"
+PYTHONUNITTESTDIR="../target/python_unit_tests"
+echo "**** Install Dependencies ****"
+$PYEXE -m pip install 'pydantic>=2.6.1,<2.10'
+$PYEXE -m pip install pytest
+echo "**** Install Runtime ****"
+$PYEXE -m pip install $MYPATH/$ROSETTARUNTIMEDIR/rosetta_runtime-2.1.0-py3-none-any.whl --force-reinstall
+echo "**** Build and Install Generated Unit Tests ****"
+cd $MYPATH/$PYTHONUNITTESTDIR
+$PYEXE -m pip wheel --no-deps --only-binary :all: . || processError
+$PYEXE -m pip install python_rosetta_dsl-0.0.0-py3-none-any.whl
 cd $MYPATH
-./runtime_tests/run_runtime_tests.sh
-./rosetta_tests/run_rosetta_tests.sh
+echo "**** Install CDM ****"
+$PYEXE -m pip install $MYPATH/$PYTHONCDMDIR/python_cdm-*-py3-none-any.whl
+
+# run tests
+$PYEXE -m pytest -p no:cacheprovider $MYPATH/runtime_tests $MYPATH/rosetta_tests $MYPATH/cdm_tests
+rm -rf .pytest
