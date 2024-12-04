a232ed92648aa97b290fb1fb09e17b270a8d0942
dschwartznyc <daniel.schwartz@ftadvisory.co>
2024-12-03 22:29:03 -0500

    clean up run_tests

--- python/test/run_tests.sh
+++ python/test/run_tests.sh
@@ -8,10 +8,6 @@ fi
 
 export PYTHONDONTWRITEBYTECODE=1
 
-ACDIR=$($PYEXE -c "import sys;print('Scripts' if sys.platform.startswith('win') else 'bin')")
-$PYEXE -m venv --clear .pytest
-source .pytest/$ACDIR/activate
-
 MYPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROSETTARUNTIMEDIR="../src/main/resources/runtime"
 PYTHONCDMDIR="../target/python"
@@ -36,8 +32,8 @@ echo "**** Install CDM ****"
 # $PYEXE -m pip install $MYPATH/$PYTHONCDMDIR/python_cdm-*-py3-none-any.whl
 
 # run tests
-$PYEXE -m pip install $MYPATH/$PYTHONCDMDIR/python_cdm-*-py3-none-any.whl
+# $PYEXE -m pip install $MYPATH/$PYTHONCDMDIR/python_cdm-*-py3-none-any.whl
 
 # run tests
-$PYEXE -m pytest -p no:cacheprovider $MYPATH/runtime_tests $MYPATH/rosetta_tests $MYPATH/cdm_tests
+$PYEXE -m pytest -p no:cacheprovider $MYPATH/runtime_tests $MYPATH/rosetta_tests # $MYPATH/cdm_tests
 rm -rf .pytest
